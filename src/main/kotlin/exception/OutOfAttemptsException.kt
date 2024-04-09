6024c2116508e0ea020badad1b7b68bbceacc587
Alexey Kharlamov <alexey.kharlamov@napier.ai>
2024-04-09 15:56:39 +0200

    added comment

--- src/main/kotlin/exception/OutOfAttemptsException.kt
+++ src/main/kotlin/exception/OutOfAttemptsException.kt
@@ -1,4 +1,7 @@
 package exception
 
+/**
+ * Exception represents situation when due to some conditions, it is not optimal to keep previous algorithm process alive
+ */
 class OutOfAttemptsException : Exception() {
 }
